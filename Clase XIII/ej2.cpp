dfdcf69b447a9087680a4ecfaae1cf5a00143b2d
Towel <78704427+Towel15@users.noreply.github.com>
2022-05-31 16:42:54 -0400

    Ej 2 Concluido Ej 3 Empezado

--- Clase XIII/ej2.cpp
+++ Clase XIII/ej2.cpp
@@ -1,4 +1,4 @@
-//@Towel15
+//@Towel15 - GITHUB
 #include <iostream>
 #include <windows.h>
 #include <locale.h>
@@ -22,19 +22,20 @@ void cargarLista(Nodo *&, Nodo *&, int, int, double);
 bool ciudadYHoraRepetida(Nodo *, int, int);
 void mostrarLista(Nodo *);
 int mostrarMenu(void);
-void registrarTemperatura(int &, int &, double &, Nodo *&, Nodo *&);
+void registrarTemperatura(int &, int &, double &, Nodo *&, Nodo *&, int &, int &);
 void consultaTemperaturasHora(Nodo *, char);
+void ciudadTempMayor(int, int);
 void consultaTodosDatos(Nodo *);
 void consultaPorCiudad(Nodo *, int);
 
 //Estructura de Nombre de las Ciudades Cargadas
 string ciudades[4] = {"Ciudad del Este", "Hernandarias", "Pdte. Franco", "Minga Guazú"};
-int cantPorCiudad[4] = {0, 0, 0, 0};
+int cantPorCiudad[4] = {0, 0, 0, 0}, cantPorHora[24] = {0}, i=0;
 
 //Funcion Principal
 main(){
 	Nodo *primero = NULL, *ultimo = NULL;
-	int ciudad, hora;
+	int ciudad, hora, ciudadMayorTemp, tempMayor;
 	double temperatura;
 	bool salir = false;
 	setlocale(LC_ALL, "spanish");
@@ -43,40 +44,55 @@ main(){
 		cout<<"Programa para registrar la temperatura.\n";
 		switch(mostrarMenu()){
 			case 1:
+				system("cls");
 				cout<<"Registrar Temperatura.\n";
-				registrarTemperatura(ciudad, hora, temperatura, primero, ultimo);
+				registrarTemperatura(ciudad, hora, temperatura, primero, ultimo, tempMayor,ciudadMayorTemp);
 				break;
 			case 2:
+				system("cls");
 				cout<<"Consulta de Temperaturas por Hora en Forma Ascendente.\n";
 				consultaTemperaturasHora(primero, 'a');
+				system("pause");
 				break;
 			case 3:
+				system("cls");
 				cout<<"Consulta de Temperaturas por Hora en Forma Descendente.\n";
+				consultaTemperaturasHora(primero, 'd');
+				system("pause");
 				break;
 			case 4:
+				system("cls");
 				cout<<"Consulta de Temperaturas Registradas por Ciudad\n";
 				cout<<"Seleccione la Ciudad cuyas temperaturas desea imprimir.\n";
 				ciudad = cargaCiudad();
 				consultaPorCiudad(primero, ciudad);
+				system("pause");
 				break;
 			case 5:
+				system("cls");
 				cout<<"Imprimir la Ciudad que Registró Mayor Temperatura en el Día\n";
+				ciudadTempMayor(tempMayor, ciudadMayorTemp);
+				system("pause");
 				break;
 			case 6:
+				system("cls");
 				consultaTodosDatos(primero);
 				system("pause");
 				break;
 			case 7:
+				system("cls");
 				cout<<"Gracias por utilizar el programa.\n";
 				system("pause");
 				salir = true;
 				break;
 			default:
-				cout<<"Carga de Temperatura.\n";
+				cout<<"Ha ocurrido un error. La opción seleccionada no está disponible   .\n";
 				break;
 		}
 	}while(!salir);
 }
+
+//Declaracion de Funciones
 
 int cargaCiudad(void){
 	int n;
@@ -120,7 +136,7 @@ double cargaTemp(void){
 	return n;
 }
 
-void registrarTemperatura(int &ciudad, int &hora, double &temperatura, Nodo *&primero, Nodo *&ultimo){
+void registrarTemperatura(int &ciudad, int &hora, double &temperatura, Nodo *&primero, Nodo *&ultimo, int &tempMayor ,int &ciudadTempMayor){
 	ciudad = cargaCiudad();
 	hora = cargaHora();
 	temperatura = cargaTemp();
@@ -134,6 +150,14 @@ void registrarTemperatura(int &ciudad, int &hora, double &temperatura, Nodo *&pr
 	}else{ 
 		cargarLista(primero, ultimo, ciudad, hora, temperatura);
 	}
+	if(i==0){
+		tempMayor=temperatura;
+		ciudadTempMayor=ciudad;
+	}else if(temperatura > tempMayor){
+		tempMayor=temperatura;
+		ciudadTempMayor=ciudad;
+	}
+	i++;
 	system("cls");
 	cout<<"Se registró la temperatura correctamente.\n";
 	system("pause");
@@ -213,7 +237,7 @@ void mostrarLista(Nodo *iniciador){
 		while(actual != NULL){
 			cout<<"Ciudad: "<<ciudades[(actual->ciudad)-1]<<"\n";
 			cout<<"Hora: "<<actual->hora<<"\n";
-			cout<<"Temperatura Registrada: "<<actual->temperatura<<"\n";
+			cout<<"Temperatura Registrada: "<<actual->temperatura<<"ºC\n";
 			actual = actual->siguiente;
 		}
 	}else{
@@ -256,29 +280,39 @@ void consultaTemperaturasHora(Nodo *primero, char op){
 		case 'a':
 			actual = primero;  
 			if(actual != NULL){
+				int horaActual=0;
 				do{
-				if(cantPorCiudad[ciudadActual] == 0){
-					ciudadActual++;
-				}else{
 					actual=primero;
-					while (actual != NULL){
-						if(actual->ciudad == ciudadActual+1){
-							cout<<"Ciudad: "<<ciudades[(actual->ciudad)-1]<<"\n";
-							cout<<"\tHora: "<<actual->hora<<"\n";
-							cout<<"\tTemperatura Registrada: "<<actual->temperatura<<"\n";
+					while(actual != NULL){
+						if(actual->hora == horaActual){
+							cout<<"Hora: "<<actual->hora<<"\n";
+							cout<<"\tTemperatura: "<<actual->temperatura<<"ºC\n";
+							cout<<"\tCiudad: "<<ciudades[(actual->ciudad) - 1]<<"\n";
 						}
 						actual = actual->siguiente;
 					}
-					ciudadActual++;
-				}
-				}while(ciudadActual < 4);
+					horaActual++;
+				}while(horaActual <= 24);
 			}else{
 				cout<<"La lista no contiene datos.\nPara imprimirlos, primero debe cargar datos en ella.\n";
 			}
 			break;
 		case 'd':
+			actual = primero;  
 			if(actual != NULL){
-				cout<<"Aun falta implementar xd";
+				int horaActual=24;
+				do{
+					actual=primero;
+					while(actual != NULL){
+						if(actual->hora == horaActual){
+							cout<<"Hora: "<<actual->hora<<"\n";
+							cout<<"\tTemperatura: "<<actual->temperatura<<"ºC\n";
+							cout<<"\tCiudad: "<<ciudades[(actual->ciudad) - 1]<<"\n";
+						}
+						actual = actual->siguiente;
+					}
+					horaActual--;
+				}while(horaActual >= 0);
 			}else{
 				cout<<"La lista no contiene datos.\nPara imprimirlos, primero debe cargar datos en ella.\n";
 			}
@@ -287,7 +321,6 @@ void consultaTemperaturasHora(Nodo *primero, char op){
 			cout<<"La opcion de Impresion No Está Disponible.\n";
 			break;
 	}
-	system("pause");
 }
 
 void consultaPorCiudad(Nodo *primero, int ciudad){
@@ -299,10 +332,10 @@ void consultaPorCiudad(Nodo *primero, int ciudad){
 			if(actual->ciudad == ciudad){
 				cout<<"Ciudad: "<<ciudades[(actual->ciudad)-1]<<"\n";
 				cout<<"\tHora: "<<actual->hora<<"\n";
-				cout<<"\tTemperatura Registrada: "<<actual->temperatura<<"\n";
-				actual = actual->siguiente;
+				cout<<"\tTemperatura Registrada: "<<actual->temperatura<<"ºC\n";
 				encontrado++;
 			}
+			actual = actual->siguiente;
 		}
 		if(encontrado == 0){
 			cout<<"La ciudad seleccionada no tiene ninguna temperatura registrada.\n";
@@ -310,10 +343,9 @@ void consultaPorCiudad(Nodo *primero, int ciudad){
 	}else{
 		cout<<"La lista no está cargada con ningún elemento.\n";
 	}
-	system("pause");
 }
 
-void consultaTodosDatos(Nodo *primero){
+void consultaTodosDatos(Nodo *primero){ 
 	Nodo *actual = new Nodo();
 	actual = primero;  
 	int ciudadActual = 0;
@@ -327,7 +359,7 @@ void consultaTodosDatos(Nodo *primero){
 					if(actual->ciudad == ciudadActual+1){
 						cout<<"Ciudad: "<<ciudades[(actual->ciudad)-1]<<"\n";
 						cout<<"\tHora: "<<actual->hora<<"\n";
-						cout<<"\tTemperatura Registrada: "<<actual->temperatura<<"\n";
+						cout<<"\tTemperatura Registrada: "<<actual->temperatura<<"ºC\n";
 					}
 					actual = actual->siguiente;
 				}
@@ -338,6 +370,11 @@ void consultaTodosDatos(Nodo *primero){
 		cout<<"La lista no contiene datos.\nPara imprimirlos, primero debe cargar datos en ella.\n";
 	}
 }
+
+void ciudadTempMayor(int tempMayor, int ciudadMayorTemp){
+	cout<<"La ciudad que registró la mayor temperatura en el día fue: "<<ciudades[(ciudadMayorTemp-1)]<<"\n";
+	cout<<"La temperatura más alta que se registró en el día fue: "<<tempMayor<<"ºC\n";
+}
 /*
 Crear un programa que permita registrar las temperaturas en °C durante el día,
 de 4 ciudades,
@@ -345,21 +382,12 @@ en donde los datos a registrar son los siguientes:
 - Temperatura
 - Ciudad (1 = Ciudad del este, 2 = Hernandarias, 3 = Pte Franco, 4 = Minga Guasu)
 - Hora (Horario 24hs)
-
 Permitir el ingreso de datos en una lista doblemente enlazada 
 y la consulta de los mismos a través del siguiente menú:
-1) Registrar Temperatura
-2) Consulta de temperaturas por hora en forma ascendente
-Ciudad 1 - Hora 1
-		   Hora 24
-Ciudad 2 - Hora 1
-		   Hora 24
-Ciudad 3 - Hora 1
-		   Hora 24
-Ciudad 4 - Hora 1
-		   Hora 24		   
-3) Consulta de temperaturas por hora en forma descendente
-4) Consulta de temperaturas registradas por ciudad
+1) Registrar Temperatura *
+2) Consulta de temperaturas por hora en forma ascendente*
+3) Consulta de temperaturas por hora en forma descendente*
+4) Consulta de temperaturas registradas por ciudad *
 5) La ciudad que registro mayor temperatura en el día
 
 Observación:
