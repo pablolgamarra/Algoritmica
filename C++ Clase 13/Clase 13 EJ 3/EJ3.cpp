e77a13d77c4bae97dbf6032bc7508fdf99221133
Towel <78704427+Towel15@users.noreply.github.com>
2022-06-04 11:49:50 -0400

    Cambio de IDE A VS

---
+++ C++ Clase 13/Clase 13 EJ 3/EJ3.cpp
@@ -0,0 +1,3 @@
+﻿Operación Compilar iniciada..
+1>------ Operación Compilar iniciada: proyecto: Clase 13 EJ 3, configuración: Debug Win32 ------
+========== Compilar: 1 correctos, 0 incorrectos, 0 actualizados, 0 omitidos ==========
