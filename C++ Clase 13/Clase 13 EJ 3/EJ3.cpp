97ec1e91d2ff49469012ec27c50e956b71f1fdc1
Towel <78704427+pablog15@users.noreply.github.com>
2022-06-16 23:37:35 -0400

    Re-Haciendo Clase 13 ej 3

--- C++ Clase 13/Clase 13 EJ 3/EJ3.cpp
+++
@@ -1,3 +0,0 @@
-﻿Operación Compilar iniciada..
-1>------ Operación Compilar iniciada: proyecto: Clase 13 EJ 3, configuración: Debug Win32 ------
-========== Compilar: 1 correctos, 0 incorrectos, 0 actualizados, 0 omitidos ==========
---
+++ Practicas/bucle para yo segundo año.cpp
@@ -0,0 +1,52 @@
+//WHILE
+//FOR
+//DO WHILE
+
+//HACER UN BUCLE DEL 1 AL 100
+// y imprimir asi--- 1,2,3,4,.....100.
+
+
+
+
+					/* li	ls
+
+					*/
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+main(){
+
+//	a   <  b	Verdadero Falso
+//	b   >  a
+//	a  <=  b
+//	a  >=  b
+//	a  ==  b
+
+
+//	int contador = 1;
+//		
+//	while(contador <= 100){
+//		cout<<contador<<"\n";
+//		contador++;
+//	}
+//	for(int contador = 1; contador <= 100; contador++){
+//		cout<<contador<<"\n ";
+//	}
+//	int contador;
+//	
+//	contador = 100;
+//			
+//	while(contador < 100){
+//		contador++;
+//		cout<<contador<<"\n";
+//	}
+	char op = 'S';
+	while(op == 's' || op == 'S'){
+		cout<<"Desea seguir? ";
+		cin>>op;		
+	}
+
+
+}
